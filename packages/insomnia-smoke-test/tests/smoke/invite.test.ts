13cf55ccb9f93034203737e25e409990cd595b50
Pavlos Koutoglou <pkoutoglou@gmail.com>
2025-02-01 01:35:35 +0100

    test: update invite test to iterate through options and improve invitation flow

--- packages/insomnia-smoke-test/tests/smoke/invite.test.ts
+++ packages/insomnia-smoke-test/tests/smoke/invite.test.ts
@@ -1,4 +1,4 @@
-import { expect } from '@playwright/test';
+// import { expect } from '@playwright/test';
 
 import { test } from '../../playwright/test';
 import { getUserEmail } from './test-utils';
@@ -7,13 +7,21 @@ const testUser = getUserEmail();
 
 test('Can invite users in app', async ({ page }) => {
   await page.getByLabel('Invite collaborators').click();
-  // have 5 members
-  await expect(page.getByLabel('Invitation list').getByRole('option')).toHaveCount(0);
+
   // invite a new member
   await page.getByPlaceholder('Enter emails, separated by').click();
   await page.getByPlaceholder('Enter emails, separated by').fill(testUser);
+
+  // Iterate through the first five options and click each one
+  for (let i = 0; i < 5; i++) {
+    const testId = `search-test-result-iteration-${i}`;
+    await page.getByTestId(testId).click();
+  }
+
+  await page.getByText('Invite collaboratorsInsomnia').click();
+
   await page.getByRole('button', { name: 'Invite', exact: true }).click();
 
-  // check if the new member is in the list
-  // await expect(page.getByRole('listbox').getByRole('option')).toHaveCount(1);
+  // Check that the new member is in the list
+  // await expect(page.getByLabel('Invitation list').getByRole('option')).toHaveCount(5);
 });
