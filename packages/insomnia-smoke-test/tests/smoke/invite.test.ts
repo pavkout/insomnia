5fd2c74bf96d1e16ffef13f21170bbf714f6e790
Pavlos Koutoglou <pkoutoglou@gmail.com>
2025-02-03 19:18:17 +0100

    refactor: update invite test to reflect changes in collaborator management and count

--- packages/insomnia-smoke-test/tests/smoke/invite.test.ts
+++ packages/insomnia-smoke-test/tests/smoke/invite.test.ts
@@ -1,4 +1,4 @@
-// import { expect } from '@playwright/test';
+import { expect } from '@playwright/test';
 
 import { test } from '../../playwright/test';
 import { getUserEmail } from './test-utils';
@@ -18,10 +18,22 @@ test('Can invite users in app', async ({ page }) => {
     await page.getByTestId(testId).click();
   }
 
-  await page.getByText('Invite collaboratorsInsomnia').click();
+  await page.getByText('Invite collaborators').click();
 
   await page.getByRole('button', { name: 'Invite', exact: true }).click();
 
   // Check that the new member is in the list
-  // await expect(page.getByLabel('Invitation list').getByRole('option')).toHaveCount(5);
+  await expect(page.getByLabel('Invitation list').getByRole('option')).toHaveCount(15);
+
+  // Change the role
+  await page.getByTestId('collaborator-test-iteration-2').getByLabel('Menu').click();
+  await page.getByLabel('admin').click();
+
+  // Delete the member
+  await page.getByTestId('collaborator-test-iteration-3').getByRole('button').nth(2).click();
+  await page.getByRole('button', { name: ' Confirm' }).click();
+
+  // Unlink the team
+  await page.getByTestId('collaborator-test-iteration-0').getByRole('button', { name: 'Remove' }).click();
+  await page.getByRole('button', { name: ' Confirm' }).click();
 });
